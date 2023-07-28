d83e60a463ad65e39735ddbb107c7d959f109c84
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2023-07-28 17:58:55 +0800

    Add files via upload

---
+++ word.js
@@ -0,0 +1,29 @@
+var words [
+    //Rumah
+    'jendela', 
+    //Dapur
+    'gelas', 'cangkir', 'sendok', 'garpu', 'sumpit', 'pisau', 'kompor', 'oven', 'kulkas', 'blender', 'talenan', 'wajan', 'piring', 'mangkok', 'beras', 'kecap', 'panci', 'garam', 'gula', 'kopi', 'teh',
+    //Kamar Mandi
+    'sabun', 'handuk', 'gayung', 'keran', 'ember',
+    //Sekolah
+    'meja', 'kursi', 'buku', 'pensil', 'penggaris', 'penghaspus', 'papan', 'poala', 'tas', 'bendera', 'lukisan', 'foto', 'piala', 'seragam', 'sepatu', 'dasi', 'topi',
+    //Hewan
+    'ayam', 'anjing', 'ikan', 'kucing', 'kelinci', 'burung', 'sapi', 'kuda', 'kerbau', 'gajah', 'unta', 'tikus', 'ular', 'cacing', 'kelelawar', 'kangguru', 'kancil', 'tupai', 'musang', 'buaya', 'pinguin', 'capung', 'ulat', 'panda', 'bebek', 'angsa', 'bangau', 'belalang', 'jangkrik', 'monyet', 'penyu', 'kura-kura', 'kupu-kupu', 'beruang', 'kambing', 'gorila', 'ubur-ubur', 'gurita', 'paus', 'hiu', 'babi', 'laba-laba',
+    //Buah
+    'apel', 'melon', 'semangka', 'pepaya', 'pisang', 'jeruk', 'durian', 'anggur', 'rambutan', 'salak', 'manggis', 'jambu', 'mangga', 'sirsak', 'lemon', 'kelengkeng', 'kelapa', 'nanas', 'belimbing', 'nangka',
+    //Sayur
+    'bayam', 'kol', 'kangkung', 'sawi',
+    //Bunga
+    'mawar', 'anggrek', 'melati',
+    //Kendaraan
+    'sepeda', 'motor', 'mobil', 'helikopter', 'pesawat', 'roket', 'kereta', 'perahu',
+    //Tempat
+    'pasar', 'kebun', 'taman', 'rumah', 'sekolah', 'gedung', 'kantor', 'pantai', 'bukit', 'gunung', 'halaman', 'parkiran', 'bandara', 'jalan', 'stadion', 'lapangan', 'klinik', 'dapur', 'restoran',
+    //Negara
+    'indonesia', 'jepang', 'jerman', 'belanda', 'spanyol', 'portugal', 'malaysia', 'india', 'palestina',
+    //Elemen
+    'api', 'udara', 'air', 'tanah', 'es',
+    
+    
+    'manusia'
+];
